9a8e85fc91ecb00faebb53eeef5f0a9f889f1d18
GAMIIX <anoj846@gmail.com>
2019-09-29 14:07:29 +0200

    backend + with auth system

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const serverkey = require("./config/keys");
+const passport = require("passport");
+const users = require("./routes/api/users");
+var morgan = require('morgan');
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+// parse application/json
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+// DB Config
+const db = serverkey.mongoURI;
+// Connect to MongoDB
+mongoose.set('useCreateIndex', true);
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB successfully connected !"))
+    .catch(err => console.log(err));
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
